fc5203d8b962e93cfccf064b1650a3d17b6b0840
DavideIsaia <davide.enjoy@hotmail.it>
2023-02-02 16:16:30 +0100

    aggiunto modello del linguaggio

---
+++ lib/providers/linguaggio.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Linguaggio with ChangeNotifier {
+  final String id;
+  final String nome;
+  final String descrizione;
+  final String logo;
+  final double progresso;
+  final Video video;
+  final Documento documenti;
+  final Collegamento collegamenti;
+
+  Linguaggio({
+    @required this.id,
+    @required this.nome,
+    @required this.descrizione,
+    @required this.logo,
+    @required this.progresso,
+    @required this.video,
+    @required this.documenti,
+    @required this.collegamenti,
+  });
+}
+
+class Video with ChangeNotifier {
+  final String link;
+  final String nome;
+  Video({
+    @required this.link,
+    @required this.nome,
+  });
+}
+
+class Documento with ChangeNotifier {
+  final String percorso;
+  final String nome;
+  final String nomeMostrato;
+  Documento({
+    @required this.percorso,
+    @required this.nome,
+    @required this.nomeMostrato,
+  });
+}
+
+class Collegamento with ChangeNotifier {
+  final String percorso;
+  final String nome;
+  Collegamento({
+    @required this.percorso,
+    @required this.nome,
+  });
+}
